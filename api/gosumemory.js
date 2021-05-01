f70962bb6b5c1c6d9a39db46dfd0044b62694f5d
Иван <56297248+valnesfjord@users.noreply.github.com>
2021-05-01 20:31:59 +0300

    WebSocket system reworked. Please copy /api/gosumemory.js to your /api/ folder and run 'npm install' in cmd in your folder

--- api/gosumemory.js
+++ api/gosumemory.js
@@ -1,12 +1,22 @@
-const WebSocket = require('ws');
+const WebSocket = require('node-reconnect-ws');
 try {
-    const ws = new WebSocket('ws://localhost:24050/ws');
-    ws.on('error', () => {
-        console.log(`[\x1b[31mERROR\x1b[0m] GOsuMemory WS connecting error. Maybe you don't start it. Start GOsuMemory and restart this app.`); 
-    });
-    module.exports = ws;
+	const ws = new WebSocket({
+		url: 'ws://localhost:24050/ws',
+		reconnectInterval: 10000,
+		autoConnect: true,
+		maxRetries: Infinity
+	});
+	ws.on('error', () => {
+		console.log(
+			`[\x1b[31mERROR\x1b[0m] GOsuMemory WS connecting error. Maybe you don't start it. Start GOsuMemory and restart this app.`
+		);
+	});
+
+	module.exports = ws;
 } catch (error) {
-    const ws = null;
-    console.log(`[\x1b[31mERROR\x1b[0m] GOsuMemory WS connecting error. Maybe you don't start it. Start GOsuMemory and restart this app.`); 
-    module.exports = ws;
+	const ws = null;
+	console.log(
+		`[\x1b[31mERROR\x1b[0m] GOsuMemory WS connecting error. Maybe you don't start it. Start GOsuMemory and restart this app.`
+	);
+	module.exports = ws;
 }
