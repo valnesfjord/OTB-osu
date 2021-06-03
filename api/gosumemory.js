6e8493c4c63e194c29f4f1bbc584f9d820d3d1d8
robloxxa <robloxxa@yandex.ru>
2021-06-04 00:09:16 +0300

    First implementaion to start gosu with app. Very questionable aswell i don't like it

--- api/gosumemory.js
+++ api/gosumemory.js
@@ -1,22 +1,43 @@
 const WebSocket = require('node-reconnect-ws');
-try {
-	const ws = new WebSocket({
-		url: 'ws://localhost:24050/ws',
-		reconnectInterval: 10000,
-		autoConnect: true,
-		maxRetries: Infinity
-	});
-	ws.on('error', () => {
-		console.log(
-			`[\x1b[31mERROR\x1b[0m] GOsuMemory WS connecting error. Maybe you don't start it. Start GOsuMemory and restart this app.`
-		);
-	});
+const folder = require('../handlers/configure').gosumemory_folder;
+const { execFile } = require('child_process');
+const ws = new WebSocket({
+	url: 'ws://localhost:24050/ws',
+	reconnectInterval: 10000,
+	autoConnect: false,
+	maxRetries: Infinity
+});
 
-	module.exports = ws;
-} catch (error) {
-	const ws = null;
-	console.log(
-		`[\x1b[31mERROR\x1b[0m] GOsuMemory WS connecting error. Maybe you don't start it. Start GOsuMemory and restart this app.`
-	);
-	module.exports = ws;
+ws.on('error', () => {
+		console.log(
+			`[\x1b[31mERROR\x1b[0m] GOsuMemory WS connecting error. Maybe you don't start it. Please start GOsuMemory.`
+		);
+});
+
+async function spawnGOSU(){
+	if(folder){
+		const path = (folder === true) ? 'gosumemory.exe' : `${folder}\\gosumemory.exe`;
+		execFile(path, (err, data) => {
+			if (err) {
+				console.log("[\x1b[31mERROR\x1b[0m] Can't open gosumemory, you probably forgot to set gosumemory folder in config.json");
+			}
+			else{
+				console.log("[\x1b[35mOTBfO\x1b[0m] Succesfully started gosumemory");
+			}
+		});
+	}
 }
+
+module.exports = {
+	ws,
+	spawnGOSU
+};
+
+
+
+
+
+
+
+
+
