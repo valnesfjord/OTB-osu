204412e2cfaec49b25cb10712130bcd4bf24d8a3
robloxxa <robloxxa@yandex.ru>
2021-06-05 04:04:09 +0300

    Changed twitch-bot package to tmi.js (Because it's better and fixes problem when bot doesn't connect to twitch on start)

--- api/twitch.js
+++ api/twitch.js
@@ -1,13 +1,36 @@
-const TwitchBot = require('twitch-bot');
 const config = require('../handlers/configure');
-const bot = new TwitchBot({
-    username: config.twitch_bot_username,
-    oauth: config.twitch_bot_token,
-    channels: [config.twitch_channel_name]
+const tmi = require('tmi.js');
+const bot = new tmi.Client({
+    connection: {
+        reconnect: true,
+        secure: true
+    },
+    identity: {
+        username: config.twitch_bot_username,
+        password: config.twitch_bot_token
+    },
+    channels: [ config.twitch_channel_name ]
 });
+
+async function TwitchConnect(){
+    return new Promise(((resolve, reject) => {
+        bot.connect()
+            .then(() => {
+                console.log(`[\x1b[35mOTBfO\x1b[0m] Twitch bot joined your channel: ${config.twitch_channel_name}`);
+                resolve();
+            })
+            .catch((err) => {
+                console.log(`[\x1b[31mERROR\x1b[0m] Twitch bot unable to start`);
+                reject(err);
+            });
+    }));
+}
 
 bot.on('error', (err) => {
     console.log(err);
 });
 
-module.exports = bot;
+module.exports = {
+    bot,
+    TwitchConnect
+};
