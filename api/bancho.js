56e7a4b074969637044f0882a10fe282e48c444c
robloxxa <robloxxa@yandex.ru>
2021-06-05 04:14:27 +0300

    add promise connect

--- api/bancho.js
+++ api/bancho.js
@@ -3,14 +3,19 @@ const Bancho = new (require('bancho.js')).BanchoClient({
     username: config.osuUsername,
     password: config.osuIRCPassword
 });
-const connect = async () => {
-    Bancho
-    .connect()
-    .then(() => {
-        console.log(`[\x1b[35mOTBfO\x1b[0m] Connected to OSU! Bancho`);
-        return Bancho.getUser(config.osuUsername);
-    })
-    .catch(console.error);
-};
+async function BanchoConnect() {
+    return new Promise(((resolve, reject) => {
+        Bancho
+            .connect()
+            .then(() => {
+                console.log(`[\x1b[35mOTBfO\x1b[0m] Connected to OSU! Bancho`);
+                resolve(Bancho.getUser(config.osuUsername));
+            })
+            .catch((err) => {
+                console.log(`[\x1b[31mERROR\x1b[0m] Can't connect to OSU! Bancho`);
+                reject(err);
+            });
+    }));
+}
 
-module.exports = connect();
+module.exports = { BanchoConnect };
