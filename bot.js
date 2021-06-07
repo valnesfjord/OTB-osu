a41193c9914032434b0a281e920cf003c049fced
robloxxa <robloxxa@yandex.ru>
2021-06-08 02:34:57 +0300

    twitch client now starts after others inits

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 (async () => {
 	await require('./api/gosumemory').spawn();
-	await require('./api/twitch').connect();
 	await require('./api/bancho').connect();
+	await require('./api/twitch').connect();
 })().catch((err) => {
 	console.error(err);
 });
